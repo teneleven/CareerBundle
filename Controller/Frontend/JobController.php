c7985b8175eba4c2a900b7858947f966cda98ae6
Justin Hilles <justin@1011i.com>
2013-11-21 14:33:24 -0800

    Add list template and controller so you can embed

--- Controller/Frontend/JobController.php
+++ Controller/Frontend/JobController.php
@@ -20,36 +20,52 @@ class JobController extends Controller
 {
 
     /**
-     * Lists all Job entities.
+     * Display Index Template
      *
      */
     public function indexAction()
     {
+        return $this->render('TenelevenCareerBundle:Frontend:index.html.twig');
+    }
+
+    /**
+     * List all Jobs
+     */
+    public function listAction($template = 'TenelevenCareerBundle:Frontend:list.html.twig')
+    {
         $em = $this->getDoctrine()->getManager();
 
-        $jobs = $em->getRepository('TenelevenCareerBundle:Job')->findAll();
+        $jobs = $em->getRepository('TenelevenCareerBundle:Job')->findBy(array(
+            'isPublished' => true
+        ));
 
-        return $this->render('TenelevenCareerBundle:Frontend:index.html.twig', array(
+        return $this->render($template, array(
             'jobs' => $jobs,
         ));
     }
 
-    public function showAction($slug)
+    /**
+     * Show Individual Job
+     */
+    public function showAction($slug, $template = 'TenelevenCareerBundle:Frontend:show.html.twig')
     {
         $em = $this->getDoctrine()->getManager();
 
-        $job = $em->getRepository('TenelevenCareerBundle:Job')->findOneBy(array('slug' => $slug));
+        $job = $em->getRepository('TenelevenCareerBundle:Job')->findOneBy(array('slug' => $slug, 'isPublished' => true));
 
         $replyForm = $this->createForm(new ReplyType(), new Reply(),array(
             'action' => $this->generateUrl('teneleven_career_frontend_reply', array('slug' => $job->getSlug()))
         ));
 
-        return $this->render('TenelevenCareerBundle:Frontend:show.html.twig', array(
+        return $this->render($template, array(
             'job' => $job, 
             'form' => $replyForm->createView()
         ));
     }
 
+    /**
+     * Handle Replies for Job
+     */
     public function replyAction($slug, Request $request)
     {
         $em = $this->getDoctrine()->getManager();
